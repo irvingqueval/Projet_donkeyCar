691107832ad929795d5a4764b2bf0ccbaa58f073
Irving Queval <irvingqueval@gmail.com>
2024-08-02 12:31:46 +0200

    search added

--- header.php
+++ header.php
@@ -9,7 +9,7 @@ $pdo = new \PDO(DSN, USER);
 <head>
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Donkye Car</title>
+  <title>Donkey Car</title>
   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
   <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
   <script src="https://code.jquery.com/jquery-3.7.1.js" integrity="sha256-eKhayi8LEQwp4NKxN+CfCh+3qOVUtJn3QNZ0TciWLP4=" crossorigin="anonymous"></script>
@@ -39,8 +39,8 @@ $pdo = new \PDO(DSN, USER);
             </ul>
           </li>
         </ul>
-        <form class="d-flex" role="search">
-          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+        <form class="d-flex" role="search" method="POST" action="/index.php">
+          <input class="form-control me-2" type="search" name="search" placeholder="search" aria-label="Search" value="<?php echo htmlspecialchars($_POST['search'] ?? ''); ?>">
           <button class="btn btn-outline-success" type="submit">Search</button>
         </form>
         <ul class="navbar-nav ms-auto mb-2 mb-lg-0">
